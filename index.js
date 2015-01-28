b41e8fc25af1b988ef3d3f2b5879c624aea6fbaf
Kevin Martensson <kevinmartensson@gmail.com>
2015-01-28 20:27:02 +0100

    Detect `.epub` without `.slice()`

--- index.js
+++ index.js
@@ -61,8 +61,7 @@ module.exports = function (buf) {
 	}
 
 	// needs to be before `zip` check
-	if (buf[0] === 0x50 && buf[1] === 0x4b && buf[2] === 0x3 && buf[3] === 0x4 &&
-		buf.slice(30, 58).toString() === 'mimetypeapplication/epub+zip') {
+	if (buf[0] === 0x50 && buf[1] === 0x4b && buf[2] === 0x3 && buf[3] === 0x4 && buf[30] === 0x6d && buf[31] === 0x69 && buf[32] === 0x6d && buf[33] === 0x65 && buf[34] === 0x74 && buf[35] === 0x79 && buf[36] === 0x70 && buf[37] === 0x65 && buf[38] === 0x61 && buf[39] === 0x70 && buf[40] === 0x70 && buf[41] === 0x6c && buf[42] === 0x69 && buf[43] === 0x63 && buf[44] === 0x61 && buf[45] === 0x74 && buf[46] === 0x69 && buf[47] === 0x6f && buf[48] === 0x6e && buf[49] === 0x2f && buf[50] === 0x65 && buf[51] === 0x70 && buf[52] === 0x75 && buf[53] === 0x62 && buf[54] === 0x2b && buf[55] === 0x7a && buf[56] === 0x69 && buf[57] === 0x70) {
 		return {
 			ext: 'epub',
 			mime: 'application/epub+zip'
