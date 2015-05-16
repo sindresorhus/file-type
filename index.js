a30198c258dc58ec5c5e02a400ce06769f7cf8eb
Sindre Sorhus <sindresorhus@gmail.com>
2015-05-16 12:28:29 +0200

    uppercase hex number for improved readability

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ module.exports = function (buf) {
 		return null;
 	}
 
-	if (buf[0] === 0xff && buf[1] === 0xd8 && buf[2] === 0xff) {
+	if (buf[0] === 0xFF && buf[1] === 0xD8 && buf[2] === 0xFF) {
 		return {
 			ext: 'jpg',
 			mime: 'image/jpeg'
 		};
 	}
 
-	if (buf[0] === 0x89 && buf[1] === 0x50 && buf[2] === 0x4e && buf[3] === 0x47) {
+	if (buf[0] === 0x89 && buf[1] === 0x50 && buf[2] === 0x4E && buf[3] === 0x47) {
 		return {
 			ext: 'png',
 			mime: 'image/png'
@@ -32,21 +32,21 @@ module.exports = function (buf) {
 		};
 	}
 
-	if ((buf[0] === 0x49 && buf[1] === 0x49 && buf[2] === 0x2a && buf[3] === 0x0) || (buf[0] === 0x4d && buf[1] === 0x4d && buf[2] === 0x0 && buf[3] === 0x2a)) {
+	if ((buf[0] === 0x49 && buf[1] === 0x49 && buf[2] === 0x2A && buf[3] === 0x0) || (buf[0] === 0x4D && buf[1] === 0x4D && buf[2] === 0x0 && buf[3] === 0x2A)) {
 		return {
 			ext: 'tif',
 			mime: 'image/tiff'
 		};
 	}
 
-	if (buf[0] === 0x42 && buf[1] === 0x4d) {
+	if (buf[0] === 0x42 && buf[1] === 0x4D) {
 		return {
 			ext: 'bmp',
 			mime: 'image/bmp'
 		};
 	}
 
-	if (buf[0] === 0x49 && buf[1] === 0x49 && buf[2] === 0xbc) {
+	if (buf[0] === 0x49 && buf[1] === 0x49 && buf[2] === 0xBC) {
 		return {
 			ext: 'jxr',
 			mime: 'image/vnd.ms-photo'
@@ -61,14 +61,14 @@ module.exports = function (buf) {
 	}
 
 	// needs to be before `zip` check
-	if (buf[0] === 0x50 && buf[1] === 0x4b && buf[2] === 0x3 && buf[3] === 0x4 && buf[30] === 0x6d && buf[31] === 0x69 && buf[32] === 0x6d && buf[33] === 0x65 && buf[34] === 0x74 && buf[35] === 0x79 && buf[36] === 0x70 && buf[37] === 0x65 && buf[38] === 0x61 && buf[39] === 0x70 && buf[40] === 0x70 && buf[41] === 0x6c && buf[42] === 0x69 && buf[43] === 0x63 && buf[44] === 0x61 && buf[45] === 0x74 && buf[46] === 0x69 && buf[47] === 0x6f && buf[48] === 0x6e && buf[49] === 0x2f && buf[50] === 0x65 && buf[51] === 0x70 && buf[52] === 0x75 && buf[53] === 0x62 && buf[54] === 0x2b && buf[55] === 0x7a && buf[56] === 0x69 && buf[57] === 0x70) {
+	if (buf[0] === 0x50 && buf[1] === 0x4B && buf[2] === 0x3 && buf[3] === 0x4 && buf[30] === 0x6D && buf[31] === 0x69 && buf[32] === 0x6D && buf[33] === 0x65 && buf[34] === 0x74 && buf[35] === 0x79 && buf[36] === 0x70 && buf[37] === 0x65 && buf[38] === 0x61 && buf[39] === 0x70 && buf[40] === 0x70 && buf[41] === 0x6C && buf[42] === 0x69 && buf[43] === 0x63 && buf[44] === 0x61 && buf[45] === 0x74 && buf[46] === 0x69 && buf[47] === 0x6F && buf[48] === 0x6E && buf[49] === 0x2F && buf[50] === 0x65 && buf[51] === 0x70 && buf[52] === 0x75 && buf[53] === 0x62 && buf[54] === 0x2B && buf[55] === 0x7A && buf[56] === 0x69 && buf[57] === 0x70) {
 		return {
 			ext: 'epub',
 			mime: 'application/epub+zip'
 		};
 	}
 
-	if (buf[0] === 0x50 && buf[1] === 0x4b && (buf[2] === 0x3 || buf[2] === 0x5 || buf[2] === 0x7) && (buf[3] === 0x4 || buf[3] === 0x6 || buf[3] === 0x8)) {
+	if (buf[0] === 0x50 && buf[1] === 0x4B && (buf[2] === 0x3 || buf[2] === 0x5 || buf[2] === 0x7) && (buf[3] === 0x4 || buf[3] === 0x6 || buf[3] === 0x8)) {
 		return {
 			ext: 'zip',
 			mime: 'application/zip'
@@ -82,28 +82,28 @@ module.exports = function (buf) {
 		};
 	}
 
-	if (buf[0] === 0x52 && buf[1] === 0x61 && buf[2] === 0x72 && buf[3] === 0x21 && buf[4] === 0x1a && buf[5] === 0x7 && (buf[6] === 0x0 || buf[6] === 0x1)) {
+	if (buf[0] === 0x52 && buf[1] === 0x61 && buf[2] === 0x72 && buf[3] === 0x21 && buf[4] === 0x1A && buf[5] === 0x7 && (buf[6] === 0x0 || buf[6] === 0x1)) {
 		return {
 			ext: 'rar',
 			mime: 'application/x-rar-compressed'
 		};
 	}
 
-	if (buf[0] === 0x1f && buf[1] === 0x8b && buf[2] === 0x8) {
+	if (buf[0] === 0x1F && buf[1] === 0x8B && buf[2] === 0x8) {
 		return {
 			ext: 'gz',
 			mime: 'application/gzip'
 		};
 	}
 
-	if (buf[0] === 0x42 && buf[1] === 0x5a && buf[2] === 0x68) {
+	if (buf[0] === 0x42 && buf[1] === 0x5A && buf[2] === 0x68) {
 		return {
 			ext: 'bz2',
 			mime: 'application/x-bzip2'
 		};
 	}
 
-	if (buf[0] === 0x37 && buf[1] === 0x7a && buf[2] === 0xbc && buf[3] === 0xaf && buf[4] === 0x27 && buf[5] === 0x1c) {
+	if (buf[0] === 0x37 && buf[1] === 0x7A && buf[2] === 0xBC && buf[3] === 0xAF && buf[4] === 0x27 && buf[5] === 0x1C) {
 		return {
 			ext: '7z',
 			mime: 'application/x-7z-compressed'
@@ -118,14 +118,14 @@ module.exports = function (buf) {
 	}
 
 	// needs to be before the `webm` check
-	if (buf[31] === 0x6d && buf[32] === 0x61 && buf[33] === 0x74 && buf[34] === 0x72 && buf[35] === 0x6f && buf[36] === 0x73 && buf[37] === 0x6b && buf[38] === 0x61) {
+	if (buf[31] === 0x6D && buf[32] === 0x61 && buf[33] === 0x74 && buf[34] === 0x72 && buf[35] === 0x6f && buf[36] === 0x73 && buf[37] === 0x6B && buf[38] === 0x61) {
 		return {
 			ext: 'mkv',
 			mime: 'video/x-matroska'
 		};
 	}
 
-	if (buf[0] === 0x1a && buf[1] === 0x45 && buf[2] === 0xdf && buf[3] === 0xa3) {
+	if (buf[0] === 0x1A && buf[1] === 0x45 && buf[2] === 0xDF && buf[3] === 0xA3) {
 		return {
 			ext: 'webm',
 			mime: 'video/webm'
@@ -146,7 +146,7 @@ module.exports = function (buf) {
 		};
 	}
 
-	if (buf[0] === 0x30 && buf[1] === 0x26 && buf[2] === 0xb2 && buf[3] === 0x75 && buf[4] === 0x8e && buf[5] === 0x66 && buf[6] === 0xcf && buf[7] === 0x11 && buf[8] === 0xa6 && buf[9] === 0xd9) {
+	if (buf[0] === 0x30 && buf[1] === 0x26 && buf[2] === 0xB2 && buf[3] === 0x75 && buf[4] === 0x8E && buf[5] === 0x66 && buf[6] === 0xCF && buf[7] === 0x11 && buf[8] === 0xA6 && buf[9] === 0xD9) {
 		return {
 			ext: 'wmv',
 			mime: 'video/x-ms-wmv'
@@ -160,28 +160,28 @@ module.exports = function (buf) {
 		};
 	}
 
-	if ((buf[0] === 0x49 && buf[1] === 0x44 && buf[2] === 0x33) || (buf[0] === 0xff && buf[1] === 0xfb)) {
+	if ((buf[0] === 0x49 && buf[1] === 0x44 && buf[2] === 0x33) || (buf[0] === 0xFF && buf[1] === 0xfb)) {
 		return {
 			ext: 'mp3',
 			mime: 'audio/mpeg'
 		};
 	}
 
-	if ((buf[4] === 0x66 && buf[5] === 0x74 && buf[6] === 0x79 && buf[7] === 0x70) || (buf[0] === 0x4d && buf[1] === 0x34 && buf[2] === 0x41 && buf[3] === 0x20)) {
+	if ((buf[4] === 0x66 && buf[5] === 0x74 && buf[6] === 0x79 && buf[7] === 0x70) || (buf[0] === 0x4D && buf[1] === 0x34 && buf[2] === 0x41 && buf[3] === 0x20)) {
 		return {
 			ext: 'm4a',
 			mime: 'audio/m4a'
 		};
 	}
 
-	if (buf[0] === 0x4f && buf[1] === 0x67 && buf[2] === 0x67 && buf[3] === 0x53) {
+	if (buf[0] === 0x4F && buf[1] === 0x67 && buf[2] === 0x67 && buf[3] === 0x53) {
 		return {
 			ext: 'ogg',
 			mime: 'audio/ogg'
 		};
 	}
 
-	if (buf[0] === 0x66 && buf[1] === 0x4c && buf[2] === 0x61 && buf[3] === 0x43) {
+	if (buf[0] === 0x66 && buf[1] === 0x4C && buf[2] === 0x61 && buf[3] === 0x43) {
 		return {
 			ext: 'flac',
 			mime: 'audio/x-flac'
@@ -202,7 +202,7 @@ module.exports = function (buf) {
 		};
 	}
 
-	if (buf[0] === 0x4d && buf[1] === 0x5a) {
+	if (buf[0] === 0x4D && buf[1] === 0x5A) {
 		return {
 			ext: 'exe',
 			mime: 'application/x-msdownload'
@@ -216,28 +216,28 @@ module.exports = function (buf) {
 		};
 	}
 
-	if (buf[0] === 0x7b && buf[1] === 0x5c && buf[2] === 0x72 && buf[3] === 0x74 && buf[4] === 0x66) {
+	if (buf[0] === 0x7B && buf[1] === 0x5C && buf[2] === 0x72 && buf[3] === 0x74 && buf[4] === 0x66) {
 		return {
 			ext: 'rtf',
 			mime: 'application/rtf'
 		};
 	}
 
-	if (buf[0] === 0x77 && buf[1] === 0x4f && buf[2] === 0x46 && buf[3] === 0x46 && buf[4] === 0x00 && buf[5] === 0x01 && buf[6] === 0x00 && buf[7] === 0x00) {
+	if (buf[0] === 0x77 && buf[1] === 0x4F && buf[2] === 0x46 && buf[3] === 0x46 && buf[4] === 0x00 && buf[5] === 0x01 && buf[6] === 0x00 && buf[7] === 0x00) {
 		return {
 			ext: 'woff',
 			mime: 'application/font-woff'
 		};
 	}
 
-	if (buf[0] === 0x77 && buf[1] === 0x4f && buf[2] === 0x46 && buf[3] === 0x32 && buf[4] === 0x00 && buf[5] === 0x01 && buf[6] === 0x00 && buf[7] === 0x00) {
+	if (buf[0] === 0x77 && buf[1] === 0x4F && buf[2] === 0x46 && buf[3] === 0x32 && buf[4] === 0x00 && buf[5] === 0x01 && buf[6] === 0x00 && buf[7] === 0x00) {
 		return {
 			ext: 'woff2',
 			mime: 'application/font-woff'
 		};
 	}
 
-	if (buf[34] === 0x4c && buf[35] === 0x50 && ((buf[8] === 0x02 && buf[9] === 0x00 && buf[10] === 0x01) || (buf[8] === 0x01 && buf[9] === 0x00 && buf[10] === 0x00) || (buf[8] === 0x02 && buf[9] === 0x00 && buf[10] === 0x02))) {
+	if (buf[34] === 0x4C && buf[35] === 0x50 && ((buf[8] === 0x02 && buf[9] === 0x00 && buf[10] === 0x01) || (buf[8] === 0x01 && buf[9] === 0x00 && buf[10] === 0x00) || (buf[8] === 0x02 && buf[9] === 0x00 && buf[10] === 0x02))) {
 		return {
 			ext: 'eot',
 			mime: 'application/octet-stream'
@@ -251,7 +251,7 @@ module.exports = function (buf) {
 		};
 	}
 
-	if (buf[0] === 0x4f && buf[1] === 0x54 && buf[2] === 0x54 && buf[3] === 0x4f && buf[4] === 0x00) {
+	if (buf[0] === 0x4F && buf[1] === 0x54 && buf[2] === 0x54 && buf[3] === 0x4F && buf[4] === 0x00) {
 		return {
 			ext: 'otf',
 			mime: 'application/font-sfnt'
@@ -265,7 +265,7 @@ module.exports = function (buf) {
 		};
 	}
 
-	if (buf[0] === 0x46 && buf[1] === 0x4c && buf[2] === 0x56 && buf[3] === 0x01) {
+	if (buf[0] === 0x46 && buf[1] === 0x4C && buf[2] === 0x56 && buf[3] === 0x01) {
 		return {
 			ext: 'flv',
 			mime: 'video/x-flv'
@@ -279,7 +279,7 @@ module.exports = function (buf) {
 		};
 	}
 
-	if (buf[0] === 0xfd && buf[1] === 0x37 && buf[2] === 0x7a && buf[3] === 0x58 && buf[4] === 0x5a && buf[5] === 0x00) {
+	if (buf[0] === 0xFD && buf[1] === 0x37 && buf[2] === 0x7A && buf[3] === 0x58 && buf[4] === 0x5A && buf[5] === 0x00) {
 		return {
 			ext: 'xz',
 			mime: 'application/x-xz'
