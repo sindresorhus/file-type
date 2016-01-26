3720c2d5c0f41f4565406ccd6a7a8f805de9c2ba
Thomas Sileghem <tsileghem@neworbit.co.uk>
2016-01-26 12:51:15 +0000

    feat(msi): detect msi file type

--- index.js
+++ index.js
@@ -414,5 +414,12 @@ module.exports = function (buf) {
 		};
 	}
 
+	if (buf[0] === 0xD0 && buf[1] === 0xCF && buf[2] === 0x11 && buf[3] === 0xE0 && buf[4] === 0xA1 && buf[5] === 0xB1 && buf[6] === 0x1A && buf[7] === 0xE1) {
+		return {
+			ext: 'msi',
+			mime: 'application/x-msi'
+		};
+	}
+
 	return null;
 };
