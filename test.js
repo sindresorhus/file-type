65f0017d8a418dd217e8eec9250c2d9f04578f56
Mikael Finstad <finstaden@gmail.com>
2017-08-15 13:37:34 +0200

    Add dash mp4
    See #60
    https://github.com/file/file/blob/master/magic/Magdir/animation

--- test.js
+++ test.js
@@ -82,7 +82,7 @@ const names = {
 	woff: ['fixture', 'fixture-otto'],
 	eot: ['fixture', 'fixture-0x20001'],
 	mov: ['fixture', 'fixture-mjpeg'],
-	mp4: ['fixture-imovie', 'fixture-isom', 'fixture-isomv2', 'fixture-mp4v2', 'fixture-m4v'],
+	mp4: ['fixture-imovie', 'fixture-isom', 'fixture-isomv2', 'fixture-mp4v2', 'fixture-m4v', 'fixture-dash'],
 	tif: ['fixture-big-endian', 'fixture-little-endian'],
 	gz: ['fixture.tar'],
 	xz: ['fixture.tar'],
