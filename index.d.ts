925ed62d6347d02329a6cd9f937f62f28848cacf
Dimitri Benin <dimitri.benin@gmail.com>
2019-03-05 01:08:02 +0100

    Add TypeScript definition

---
+++ index.d.ts
@@ -0,0 +1,122 @@
+/// <reference types="node"/>
+
+export type FileType =
+	| 'jpg'
+	| 'png'
+	| 'gif'
+	| 'webp'
+	| 'flif'
+	| 'cr2'
+	| 'tif'
+	| 'bmp'
+	| 'jxr'
+	| 'psd'
+	| 'zip'
+	| 'tar'
+	| 'rar'
+	| 'gz'
+	| 'bz2'
+	| '7z'
+	| 'dmg'
+	| 'mp4'
+	| 'm4v'
+	| 'mid'
+	| 'mkv'
+	| 'webm'
+	| 'mov'
+	| 'avi'
+	| 'wmv'
+	| 'mpg'
+	| 'mp2'
+	| 'mp3'
+	| 'm4a'
+	| 'ogg'
+	| 'opus'
+	| 'flac'
+	| 'wav'
+	| 'qcp'
+	| 'amr'
+	| 'pdf'
+	| 'epub'
+	| 'mobi'
+	| 'exe'
+	| 'swf'
+	| 'rtf'
+	| 'woff'
+	| 'woff2'
+	| 'eot'
+	| 'ttf'
+	| 'otf'
+	| 'ico'
+	| 'flv'
+	| 'ps'
+	| 'xz'
+	| 'sqlite'
+	| 'nes'
+	| 'crx'
+	| 'xpi'
+	| 'cab'
+	| 'deb'
+	| 'ar'
+	| 'rpm'
+	| 'Z'
+	| 'lz'
+	| 'msi'
+	| 'mxf'
+	| 'mts'
+	| 'wasm'
+	| 'blend'
+	| 'bpg'
+	| 'docx'
+	| 'pptx'
+	| 'xlsx'
+	| '3gp'
+	| 'jp2'
+	| 'jpm'
+	| 'jpx'
+	| 'mj2'
+	| 'aif'
+	| 'odt'
+	| 'ods'
+	| 'odp'
+	| 'xml'
+	| 'heic'
+	| 'cur'
+	| 'ktx'
+	| 'ape'
+	| 'wv'
+	| 'asf'
+	| 'wma'
+	| 'wmv'
+	| 'dcm'
+	| 'mpc'
+	| 'ics'
+	| 'glb'
+	| 'pcap';
+
+export interface FileTypeResult {
+	/**
+	 * One of the supported [file types](https://github.com/BendingBender/file-type#supported-file-types).
+	 */
+	ext: FileType;
+
+	/**
+	 * The detected [MIME type](http://en.wikipedia.org/wiki/Internet_media_type).
+	 */
+	mime: string;
+}
+
+/**
+ * Detect the file type of a `Buffer`/`Uint8Array`. The file type is detected by checking the [magic number](http://en.wikipedia.org/wiki/Magic_number_(programming)#Magic_numbers_in_files) of the buffer.
+ *
+ * @param buffer - It only needs the first `.minimumBytes` bytes. The exception is detection of `docx`, `pptx`, and `xlsx` which potentially requires reading the whole file.
+ * @returns An object with the detected file type and MIME type or `null` when there was no match.
+ */
+export default function fileType(
+	buffer: Buffer | Uint8Array
+): FileTypeResult | null;
+
+/**
+ * The minimum amount of bytes needed to detect a file type. Currently, it's 4100 bytes, but it can change, so don't hard-code it.
+ */
+export const minimumBytes: number;
---
+++ index.test-d.ts
@@ -0,0 +1,13 @@
+import {expectType} from 'tsd-check';
+import fileType, {FileTypeResult, minimumBytes, FileType} from '.';
+
+expectType<FileTypeResult | null>(fileType(new Buffer([0xff, 0xd8, 0xff])));
+expectType<FileTypeResult | null>(fileType(new Uint8Array([0xff, 0xd8, 0xff])));
+
+const result = fileType(new Buffer([0xff, 0xd8, 0xff]));
+if (result != null) {
+	expectType<FileType>(result.ext);
+	expectType<string>(result.mime);
+}
+
+expectType<number>(minimumBytes);
