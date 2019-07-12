a935ba80c74c8ef40a9e3110b79ffd8c6414ea2b
Aditya Patadia <adityapatadia@users.noreply.github.com>
2019-07-12 11:03:04 +0530

    Update index.test-d.ts

--- index.test-d.ts
+++ index.test-d.ts
@@ -15,6 +15,10 @@ if (result !== undefined) {
 
 expectType<number>(fileType.minimumBytes);
 
+expectType<object>(fileType.extensions);
+
+expectType<object>(fileType.mimeTypes);
+
 const readableStream = fs.createReadStream('file.png');
 const streamWithFileType = fileType.stream(readableStream);
 expectType<Promise<ReadableStreamWithFileType>>(streamWithFileType);
