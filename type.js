021ee8841ec93fd91d514ee03c745c47b4c98471
Borewit <borewit@users.noreply.github.com>
2019-12-22 19:20:24 +0100

    #263: Add Node.js script to analyze single file

---
+++ type.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const FileType = require('.');
+
+if (process.argv.length !== 3) {
+	console.error('Expected path of the file to examine');
+	process.exit();
+}
+
+const file2test = process.argv[2];
+
+(async () => {
+	const fileType = await FileType.fromFile(file2test);
+
+	if (fileType) {
+		console.log(`MIME-type: ${fileType.mime}`);
+		console.log(`extension: ${fileType.ext}`);
+	} else {
+		console.log('Could not determine file type.');
+	}
+})();
