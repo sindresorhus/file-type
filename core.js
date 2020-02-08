e2d9138b2899cbcf90797ea1591ec33788a4a0af
Patrick Hulce <patrick.hulce@gmail.com>
2020-02-08 12:05:57 -0600

    feedback

--- core.js
+++ core.js
@@ -581,10 +581,9 @@ async function _fromTokenizer(tokenizer) {
 		};
 	}
 
-	// TIFF-compatible formats can be encoded two ways:
-	//  - little-endian/Intel (0x49)
-	//  - big-endian/Motorola (0x4D)
-	if (check([0x49, 0x49, 0x2A, 0x0]) || check([0x4D, 0x4D, 0x0, 0x2A])) {
+	// TIFF-compatible formats can be encoded two ways but little-endian (0x49) is most common.
+	// Big-endian TIFFs are handled below this block.
+	if (check([0x49, 0x49, 0x2A, 0x0])) {
 		if (checkString('CR', {offset: 8})) {
 			return {
 				ext: 'cr2',
