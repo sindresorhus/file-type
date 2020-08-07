5543e3131374bdcc0fda81ecb0e2b7bbff9cbbec
Borewit <borewit@users.noreply.github.com>
2020-08-07 08:33:14 +0200

    Simplify tar checksum calculation.

--- util.js
+++ util.js
@@ -2,43 +2,32 @@
 
 exports.stringToBytes = string => [...string].map(character => character.charCodeAt(0));
 
-exports.tarHeaderChecksumMatches = buffer => { // Does not check if checksum field characters are valid
-	if (buffer.length < 512) { // `tar` header size, cannot compute checksum without it
-		return false;
-	}
+/**
+ * Return true if the tar checksum is valid
+ * @param {Buffer} buffer holding tar header [offset ... offset + 512]
+ * @param {number} off TAR header offset
+ * @returns {boolean} rue if the tar checksum is valid, otherwise false
+ */
+exports.tarHeaderChecksumMatches = (buffer, off) => {
+	off = off ? off : 0;
 
 	const readSum = parseInt(buffer.toString('utf8', 148, 154).replace(/\0.*$/, '').trim(), 8); // Read sum in header
 	if (isNaN(readSum)) {
 		return false;
 	}
 
-	const MASK_8TH_BIT = 0x80;
+	let sum = 8 * 0x20; // Initialize signed bit sum
 
-	let sum = 256; // Initialize sum, with 256 as sum of 8 spaces in checksum field
-	let signedBitSum = 0; // Initialize signed bit sum
-
-	for (let i = 0; i < 148; i++) {
-		const byte = buffer[i];
-		sum += byte;
-		signedBitSum += byte & MASK_8TH_BIT; // Add signed bit to signed bit sum
+	for (let i = off; i < off + 148; i++) {
+		sum += buffer[i];
 	}
 
-	// Skip checksum field
-
-	for (let i = 156; i < 512; i++) {
-		const byte = buffer[i];
-		sum += byte;
-		signedBitSum += byte & MASK_8TH_BIT; // Add signed bit to signed bit sum
+	for (let i = off + 156; i < off + 512; i++) {
+		sum += buffer[i];
 	}
 
 	// Some implementations compute checksum incorrectly using signed bytes
-	return (
-		// Checksum in header equals the sum we calculated
-		readSum === sum ||
-
-		// Checksum in header equals sum we calculated plus signed-to-unsigned delta
-		readSum === (sum - (signedBitSum << 1))
-	);
+	return readSum === sum;
 };
 
 /**
