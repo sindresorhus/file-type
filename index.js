29930bf38626d452564f5691d11e63a27e3f0edf
FredrikSchaefer <67001822+FredrikSchaefer@users.noreply.github.com>
2023-07-25 09:27:01 +0200

    Adjust fileTypeFromFile(...) to recent changes

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import * as strtok3 from 'strtok3';
 import {fileTypeFromTokenizer} from './core.js';
 
-export async function fileTypeFromFile(path, customDetectors) {
+export async function fileTypeFromFile(path, fileTypeOptions) {
 	const tokenizer = await strtok3.fromFile(path);
 	try {
-		return await fileTypeFromTokenizer(tokenizer, customDetectors);
+		return await fileTypeFromTokenizer(tokenizer, fileTypeOptions);
 	} finally {
 		await tokenizer.close();
 	}
