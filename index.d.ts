259a97dc002532fbcf8012312d0d78bcf2b8a183
Sindre Sorhus <sindresorhus@gmail.com>
2024-07-06 17:31:27 +0200

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -9,20 +9,19 @@ import {FileTypeParser} from './core.js';
 
 export declare class NodeFileTypeParser extends FileTypeParser {
 	/**
-	 *
-	 * @param stream Node.js Stream readable or Web API StreamReadable
-	 */
+	@param stream Node.js Stream readable or Web API StreamReadable
+	*/
 	fromStream(stream: WebReadableStream<Uint8Array> | NodeReadableStream): Promise<FileTypeResult | undefined>;
 }
 
 /**
- * Detect the file type of a file path.
- *
- * The file type is detected by checking the [magic number](https://en.wikipedia.org/wiki/Magic_number_(programming)#Magic_numbers_in_files) of the buffer.
- *
- * @param path
- * @returns The detected file type and MIME type or `undefined` when there is no match.
- */
+Detect the file type of a file path.
+
+The file type is detected by checking the [magic number](https://en.wikipedia.org/wiki/Magic_number_(programming)#Magic_numbers_in_files) of the buffer.
+
+@param path
+@returns The detected file type and MIME type or `undefined` when there is no match.
+*/
 export function fileTypeFromFile(path: string): Promise<FileTypeResult | undefined>;
 
 export function fileTypeFromStream(stream: WebReadableStream<Uint8Array> | NodeReadableStream): Promise<FileTypeResult | undefined>;
