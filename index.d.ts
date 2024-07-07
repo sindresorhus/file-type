4e206691bb5351e857d0d301859719e48038f9b9
Sindre Sorhus <sindresorhus@gmail.com>
2024-07-07 23:08:31 +0200

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -47,7 +47,7 @@ A smaller sample size will result in lower probability of the best file type det
 
 @param readableStream - A [readable stream](https://nodejs.org/api/stream.html#stream_class_stream_readable) containing a file to examine.
 @param options - Maybe used to override the default sample-size.
- @returns A `Promise` which resolves to the original readable stream argument, but with an added `fileType` property, which is an object like the one returned from `fileTypeFromFile()`.
+@returns A `Promise` which resolves to the original readable stream argument, but with an added `fileType` property, which is an object like the one returned from `fileTypeFromFile()`.
 
 @example
 ```
